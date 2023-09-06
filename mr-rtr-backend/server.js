ba4cf77ec3747f432087f09ac9250025e2fc1d4c
Mukesh-Thidiff <mukesh.murugaiyan@thidiff.com>
2023-09-06 11:57:13 +0530

    frontend to backend values send.

--- mr-rtr-backend/server.js
+++ mr-rtr-backend/server.js
@@ -1,16 +1,29 @@
-const express = require("express");
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors');
+
 const app = express();
+const port = 3002;
 
-const users = [
-  { id: 1, name: "John" },
-  { id: 2, name: "Jane" },
-  { id: 3, name: "Bob" },
-];
+app.use(cors());
 
-app.get("/api", (req, res) => {
-  res.send({data:users});
+app.use(express.json());
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.post("/v1/user/signup", upload.any(), (req, res) => {
+  try {
+    console.log('Data from form:', req.body);
+
+    res.json(200,{ message: 'Form data received successfully' });
+  } catch (error) {
+    console.error('Error handling form data:', error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
 });
 
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
