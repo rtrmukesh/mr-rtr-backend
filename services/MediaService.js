eb4bbb5776a875ad3c5cfa8ae9183c9e39c1e1eb
Mukesh-Thidiff <mukesh.murugaiyan@gmail.com>
2024-11-21 10:53:49 +0530

    dd

--- services/MediaService.js
+++ services/MediaService.js
@@ -1,68 +1,28 @@
-const path = require('path');
-const youtubedl = require('youtube-dl-exec');
+const ytdlp = require('yt-dlp-exec');
 
-class VideoService {
-
-    static getVideoQualities(videoUrl) {
-        return new Promise((resolve, reject) => {
-            youtubedl(videoUrl, {
-                cookies: path.resolve('./chromewebstore.google.com_cookies.txt'), // Path to your cookies file
+class MediaService {
+    static async getQualitiy(req, res, next) {
+        const videoUrl = req.query.url;
+        try {
+            const output = await ytdlp(videoUrl, {
                 dumpSingleJson: true,
                 noWarnings: true,
-                format: 'all', // Fetch all available formats
-            })
-                .then((info) => {
-                    console.log('Cookie path:', path.resolve('./chromewebstore.google.com_cookies.txt'));
+                noCallHome: true,
+                format: 'bestaudio/best'
+            });
+            const qualities = output.formats.map(file => ({
+                id: file.format_id,
+                label: file.format_note,
+                resolution: file.height ? `${file.height}p` : 'Audio',
+                url: file.url
+            }));
+            res.status(200).json({ qualities });
+        } catch (error) {
+            console.error('Error:', error);
+            res.json(400, 'Error fetching qualities');
+        }
 
-                    const formats = info.formats
-                        .filter((file) => file.url) // Ensure only formats with URLs are included
-                        .map((file) => ({
-                            id: file.format_id,
-                            label: file.format_note || 'Unknown',
-                            resolution: file.vcodec && file.height ? `${file.height}p` : 'Audio',
-                            url: file.url,
-                            format: file.mimeType || 'Unknown',
-                        }));
-                    resolve(formats);
-                })
-                .catch((err) => {
-                    console.error('Error fetching qualities:', err);
-                    reject(err);
-                });
-        });
-    }
-
-
-    // static downloadVideo(videoUrl, formatId, outputDir) {
-    //     return new Promise((resolve, reject) => {
-    //         const outputFilePath = path.join(outputDir, '%(title)s.%(ext)s');
-
-    //         youtubedl(videoUrl, {
-    //             format: formatId,
-    //             output: outputFilePath,
-    //         })
-    //             .then((output) => {
-    //                 console.log('Video downloaded successfully:', output);
-    //                 resolve(output);
-    //             })
-    //             .catch((err) => {
-    //                 console.error('Error downloading video:', err);
-    //                 reject(err);
-    //             });
-    //     });
-    // }
-
-    static async getQualitiy(req, res, next) {
-        let videoUrl = req.query.url;
-        VideoService.getVideoQualities(videoUrl)
-            .then((qualities) => {
-                return res.status(200).json({ qualities });
-            })
-            .catch((err) => console.error('Error:', err));
     }
 }
 
-module.exports = VideoService
-
-
-
+module.exports = MediaService;
